19929f72907f8b846f8117cba50d3a7bad74b56b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-04-16 10:21:35 -0400

    change AddTodo to class for error state

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AddTodo.css';
 
-const AddTodo = ({ handleAddTodo }) => {
+class AddTodo extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      error : undefined
+    }
+  }
+
   /**
    * @desc function to pass input value to handleAddTodo
    * @param string $e - the input value
   **/
-  const onAdd = (e) => {
+  onAdd = (e) => {
     e.preventDefault();
-    const input = e.target.add.value.trim();
-    handleAddTodo(input);
-    e.target.add.value = '';
-  }
-  return (
-    <form onSubmit={onAdd} id="add-todo-form">
-      <input type="text" name="add" placeholder="add todo" required/>
-      <button>Add</button>
-    </form>
-  );
-};
 
+    const input = e.target.add.value.trim();
+
+    const error = this.props.handleAddTodo(input);
+    this.setState({ error });
+
+    if (!error) {
+      e.target.add.value = '';
+    }
+  }
+
+  render() {
+    return (
+      <div id="add-todo">
+        <form onSubmit={this.onAdd} id="add-todo-form">
+          <input type="text" name="add" placeholder="add todo" required />
+          <button>Add</button>
+        </form>
+
+        {this.state.error && <p class="error-msg">{this.state.error}</p>}
+      </div>
+    );
+  }
+}
 
 export default AddTodo;
