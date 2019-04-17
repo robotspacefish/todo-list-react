0f654d1e43424344be199d86ca0133b3cf1148b2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-04-17 13:45:45 -0400

    reverse bang IDs to match values passed to handleRating

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -4,11 +4,11 @@ import './Rating.css';
 const Rating = ({ handleRating }) => {
   return (
     <div id="ratings">
-      <span onClick={() => handleRating(5)} className="rating" id="rating-1">!</span>
-      <span onClick={() => handleRating(4)} className="rating" id="rating-2">!</span>
+      <span onClick={() => handleRating(5)} className="rating" id="rating-5">!</span>
+      <span onClick={() => handleRating(4)} className="rating" id="rating-4">!</span>
       <span onClick={() => handleRating(3)} className="rating" id="rating-3">!</span>
-      <span onClick={() => handleRating(2)} className="rating" id="rating-4">!</span>
-      <span onClick={() => handleRating(1)} className="rating" id="rating-5">!</span>
+      <span onClick={() => handleRating(2)} className="rating" id="rating-2">!</span>
+      <span onClick={() => handleRating(1)} className="rating" id="rating-1">!</span>
     </div>
   );
 };
