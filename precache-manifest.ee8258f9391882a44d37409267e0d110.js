b6df4035b7fc96b67d99826abb79c049ecacc6ea
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-04-17 14:29:03 -0400

    Updates

---
+++ precache-manifest.ee8258f9391882a44d37409267e0d110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7be31a8c5408566b26fb",
+    "url": "/todo-list-react/static/css/main.b9a68cc5.chunk.css"
+  },
+  {
+    "revision": "7be31a8c5408566b26fb",
+    "url": "/todo-list-react/static/js/main.51a3d8e7.chunk.js"
+  },
+  {
+    "revision": "2e03235dbed8696c067c",
+    "url": "/todo-list-react/static/js/runtime~main.f99d2fcc.js"
+  },
+  {
+    "revision": "3562454454aceb3ec0ba",
+    "url": "/todo-list-react/static/js/2.0264c4d0.chunk.js"
+  },
+  {
+    "revision": "0241c2b9d9e06fcf613030fa9e3f716e",
+    "url": "/todo-list-react/index.html"
+  }
+];
