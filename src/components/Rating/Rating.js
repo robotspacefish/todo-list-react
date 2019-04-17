b02909819baef051cd52d6fbb1671dc205b5e4ff
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-04-17 12:28:44 -0400

    change rating bangs from buttons to spans to prevent form submission

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -4,11 +4,11 @@ import './Rating.css';
 const Rating = ({ handleRating }) => {
   return (
     <div id="ratings">
-      <button onClick={() => handleRating(5)} className="rating" id="rating-1">!</button>
-      <button onClick={() => handleRating(4)} className="rating" id="rating-2">!</button>
-      <button onClick={() => handleRating(3)} className="rating" id="rating-3">!</button>
-      <button onClick={() => handleRating(2)} className="rating" id="rating-4">!</button>
-      <button onClick={() => handleRating(1)} className="rating" id="rating-5">!</button>
+      <span onClick={() => handleRating(5)} className="rating" id="rating-1">!</span>
+      <span onClick={() => handleRating(4)} className="rating" id="rating-2">!</span>
+      <span onClick={() => handleRating(3)} className="rating" id="rating-3">!</span>
+      <span onClick={() => handleRating(2)} className="rating" id="rating-4">!</span>
+      <span onClick={() => handleRating(1)} className="rating" id="rating-5">!</span>
     </div>
   );
 };
